43d777ad2bedfac1e9002d72c839d17d534b239d
Daniil-7 <yas66yelchili@gmail.com>
2022-10-30 21:40:42 +0300

    add form in profile

--- app/config/settings.py
+++ app/config/settings.py
@@ -74,8 +74,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-# prod launch
-
+# deploy launch
 DATABASES = {                                         
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -149,7 +148,8 @@ EMAIL_USE_SSL = False
 DEFAULT_FROM_EMAIL = "admin@psb-hosting.pro"
 SERVER_EMAIL = "admin@psb-hosting.pro"
 
-
+# deploy
+CSRF_TRUSTED_ORIGINS=['https://psb-hosting.pro']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
